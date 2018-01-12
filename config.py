d3b9cf0306c13621b16a972976293706e11f7b34
shusongsong <hesongl@andrew.cmu.edu>
2018-01-12 16:14:44 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+SRF_ENABLED = True
+SECRET_KEY = 'heslngl_qj_create'
+
+MYSQLCONFIG = {
+	'host':'127.0.0.1',
+	'port':3306,
+	'user':'root',
+	'password':'Asong_89757',
+	'db':'oj',
+	'charset':'utf8'
+}
+
